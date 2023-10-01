0eb241a08f16da3fad1d811e8151c34099033195
Hyungjun <dlgudwns1207@gmail.com>
2023-10-02 06:44:34 +0900

    Feat: Bunny image

--- src/Components/DrawHangMan.js
+++ src/Components/DrawHangMan.js
@@ -1,82 +1,18 @@
 import { Fragment } from "react";
 import useStore from "../Store/Store";
+import bunny1 from "../Images/송편토끼-01.jpg";
+import bunny2 from "../Images/송편토끼-02.jpg";
+import bunny3 from "../Images/송편토끼-03.jpg";
+import bunny4 from "../Images/송편토끼-04.jpg";
+import bunny5 from "../Images/송편토끼-05.jpg";
+import bunny6 from "../Images/송편토끼-06.jpg";
+import bunny7 from "../Images/송편토끼-07.jpg";
+import bunny8 from "../Images/송편토끼-08.jpg";
+
+const hangmanArt = [bunny1, bunny2, bunny3, bunny4, bunny5, bunny6, bunny7, bunny8];
 
 const DrawHangman = () => {
     const { mistakeCount } = useStore();
-    const hangmanArt = [
-        `
-    +---+
-        |
-        |
-        |
-        |
-        |
-  =========
-    `,
-        `
-    +---+
-    |   |
-        |
-        |
-        |
-        |
-  =========
-    `,
-        `
-    +---+
-    |   |
-    O   |
-        |
-        |
-        |
-  =========
-    `,
-        `
-    +---+
-    |   |
-    O   |
-    |   |
-        |
-        |
-  =========
-    `,
-        `
-    +---+
-    |   |
-    O   |
-   /|   |
-        |
-        |
-  =========
-    `,
-        `
-    +---+
-    |   |
-    O   |
-   /|\\  |
-        |
-        |
-  =========
-    `,
-        `
-    +---+
-    |   |
-    O   |
-   /|\\  |
-   /    |
-        |
-  =========
-    `,
-        `
-    +---+
-    |   |
-    O   |
-   /|\\  |
-   / \\  |
-        |
-  =========
-    `,
-    ];
 
     const getHangmanArt = () => {
         if (mistakeCount >= 0 && mistakeCount < hangmanArt.length) {
@@ -88,7 +24,11 @@ const DrawHangman = () => {
 
     return (
         <Fragment>
-            <pre style={{ fontFamily: "Courier New" }}>{getHangmanArt()}</pre>
+            <img
+                src={getHangmanArt()}
+                alt={`Hangman Art ${mistakeCount}`}
+                style={{ maxWidth: "10%", height: "auto" }}
+            />
         </Fragment>
     );
 };
